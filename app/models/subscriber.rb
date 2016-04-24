ccc938c2186a36d798284123c1cd7b1e84b5eb75
cam@rowstercoffee.com <cam@collectiveidea.com>
2016-04-23 23:44:50 -0400

    add date_joined to model

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -3,4 +3,8 @@ class Subscriber < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true
   validates :phone_number, presence: true
+
+  def date_joined
+    created_at.strftime("%-m/%-d/%-y")
+  end
 end
