54fea69d84497cb4b0c2924649270205a577d3c8
cam@rowstercoffee.com <cam@collectiveidea.com>
2016-04-27 10:08:24 -0400

    add RSpec

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -7,4 +7,8 @@ class Subscriber < ActiveRecord::Base
   def date_joined
     created_at.strftime("%-m/%-d/%-y")
   end
+
+  def expiration_date
+    (created_at + 1.year).strftime("%-m/%-d/%-y")
+  end
 end
