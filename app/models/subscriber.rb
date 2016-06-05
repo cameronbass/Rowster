af2a6dc1f2ef68a415b34813630c774817b2335e
cam@rowstercoffee.com <cam@collectiveidea.com>
2016-06-04 20:37:57 -0400

    add regex for email

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,7 +1,8 @@
 class Subscriber < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, :presence => true, :uniqueness => true, :format => VALID_EMAIL_REGEX
   validates :phone_number, presence: true
 
   def date_joined
