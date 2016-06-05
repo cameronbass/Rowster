e4d1189b14139af7aa2fa16d46f1d6b1b6aca0ea
cam@rowstercoffee.com <cam@collectiveidea.com>
2016-06-05 00:06:50 -0400

    fix typo on validations

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -3,8 +3,15 @@ class Subscriber < ActiveRecord::Base
   VALID_PHONE_REGEX = /\d{7}/
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, :presence => true, :uniqueness => true, :format => VALID_EMAIL_REGEX
-  validates :phone_number,:presence => true, :numericality => true, :format => VALID_PHONE_REGEX
+  validates :email,
+            :presence => true,
+            :uniqueness => true,
+            :format => VALID_EMAIL_REGEX
+  validates :phone_number,
+            :presence => true,
+            :uniqueness => true,
+            :numericality => true,
+            :format => VALID_PHONE_REGEX
 
   def date_joined
     created_at.strftime("%-m/%-d/%-y")
