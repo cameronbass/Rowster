5ec6b92f2cda7e3bb4b0670b52a87901c3410c56
Cameron Bass <cam@collectiveidea.com>
2016-06-20 20:02:34 -0400

    add test for comment view

---
+++ spec/features/create_comment_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+RSpec.feature "Create a Comment" do
+  scenario "Customer can leave additional information" do
+    visit "/comments/new"
+
+    user = FactoryGirl.create(:user)
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Sign in"
+
+    subscriber = FactoryGirl.create(:subscriber)
+    fill_in "comment_fav_drink", with: "Latte"
+    click_button "Send"
+
+    expect(page).to have_content("Thank you!")
+  end
+end
