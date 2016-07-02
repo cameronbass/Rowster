f40f98be222238dfd8753d31396f23badd1db079
Cameron Bass <cam@collectiveidea.com>
2016-07-02 10:30:06 -0400

    Clean up on the Subscriber model

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,20 +1,16 @@
 class Subscriber < ActiveRecord::Base
-  has_many :comments
   before_create :generate_mug_number
+
+  scope :subscribing, -> {where subscription_date > 1.year.ago}
+  scope :non_subscribing, -> {where.not subscription_date > 1.year.ago}
+
+  has_many :comments
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   VALID_PHONE_REGEX = /\d{10}/
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email,
-            :presence => true,
-            :uniqueness => true,
-            :format => VALID_EMAIL_REGEX
-  validates :phone_number,
-            :presence => true,
-            :uniqueness => true,
-            :numericality => true,
-            :format => VALID_PHONE_REGEX
-
+  validates :email, :presence => true, :uniqueness => true, :format => VALID_EMAIL_REGEX
+  validates :phone_number, :presence => true, :uniqueness => true, :numericality => true, :format => VALID_PHONE_REGEX
 
   def date_joined
     created_at.strftime("%-m/%-d/%-y")
@@ -26,6 +22,10 @@ class Subscriber < ActiveRecord::Base
 
   def days_till_expired
     ((created_at + 1.year) - DateTime.now).to_i / 1.day
+  end
+
+  def subscribing?
+    subscription_date > 1.year.ago
   end
 
   private
