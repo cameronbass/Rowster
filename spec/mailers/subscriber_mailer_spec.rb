707f6416d976e5f0c615c65c0027af9319a41e8d
Cameron Bass <cam@collectiveidea.com>
2016-07-03 20:29:42 -0400

    test suite is green

--- spec/mailers/subscriber_mailer_spec.rb
+++ spec/mailers/subscriber_mailer_spec.rb
@@ -1,18 +1,1 @@
-require "rails_helper"
 
-RSpec.describe SubscriberMailer, type: :mailer do
-  describe "welcome_subscriber" do
-    let(:mail) { SubscriberMailer.welcome_subscriber }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Welcome subscriber")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
-  end
-
-end
