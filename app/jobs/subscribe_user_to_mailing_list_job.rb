ca62630002d532a046692db55539fcda51192ac9
Cameron Bass <cam@collectiveidea.com>
2016-07-05 21:40:31 -0400

    create show.js page for modal

--- app/jobs/subscribe_user_to_mailing_list_job.rb
+++ app/jobs/subscribe_user_to_mailing_list_job.rb
@@ -2,7 +2,7 @@ class SubscribeUserToMailingListJob < ActiveJob::Base
   queue_as :default
 
   def perform(subscriber)
-    gb = Gibbon::API.new(Figaro.env.mailchimp_api_key)
+    gb = Gibbon::API.new(ENV["MAILCHIMP_API_KEY"])
     gb.lists.subscribe({:id => ENV["MAILCHIMP_LIST_ID"], :email => {:email => subscriber.email}, :merge_vars => {:FNAME => subscriber.first_name, :LNAME => subscriber.last_name}, :double_optin => false})
   end
 end
