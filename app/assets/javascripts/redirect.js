748022fbb865f7aa1ee8b0a25a497cd162fcc8da
Cameron Bass <cam@collectiveidea.com>
2016-08-28 19:34:55 -0400

    Add if statement to redirect.js

--- app/assets/javascripts/redirect.js
+++ app/assets/javascripts/redirect.js
@@ -1,9 +1,11 @@
-var redirectTimeoutId = window.setTimeout(redirectToHomepage, 15000)
+if(window.location.pathname == "/subscribers/visit") {
+  var redirectTimeoutId = window.setTimeout(redirectToHomepage, 15000)
 
-function redirectToHomepage() {
-  window.location.href = '/subscribers/search'; // or whatever your homepage would be
+  function redirectToHomepage() {
+    window.location.href = '/subscribers/search'; // or whatever your homepage would be
+  }
+
+  window.addEventListener('click keypress mousemove', function() {
+    window.clearTimeout(redirectTimeoutId)
+  }, true)
 }
-
-window.addEventListener('click keypress mousemove', function() {
-  window.clearTimeout(redirectTimeoutId)
-}, true)
